d606de53ddeb8fd565006a89571f0132e867f6d1
gliese876b <alper91demir@gmail.com>
2025-09-24 10:07:38 +0300

    exp3

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -33,3 +33,25 @@ df_experiment02 = df_experiment02.rename(columns={'replica':'num_containers',
 df_experiment02.to_csv('../data/nsfw_experiment2.csv',index=False)
 print('Saved ../data/nsfw_experiment2.csv')
 
+
+#############################
+# Data prep for Experiment03
+#############################
+# take subset of df where cpu=40
+print(f'Preparing experiment03 data...fixing a specific cpu and varying expected_tps')
+
+df_experiment03 = df.copy()
+df_experiment03["previous_expected_tps"] = df_experiment03["expected_tps"].shift(fill_value=1)
+
+df_experiment03 = df_experiment03[(df['cpu'] == 40)] 
+
+# drop all columns except replica, cpu_usage and expected_tps
+df_experiment03 = df_experiment03[['replica', 'expected_tps', 'instant_tps', 'cpu_utilization', 'previous_expected_tps', 'response_time']]
+# rename replica to num_containers, cpu_usage to avg_cpu_util and expected_tps to arrival_rate
+df_experiment03 = df_experiment03.rename(columns={'replica':'num_containers',
+                                      'cpu_usage':'avg_cpu_util',
+                                      'expected_tps':'arrival_rate',
+                                      'previous_expected_tps':'prev_arrival_rate'})
+# Data for Experiment2
+df_experiment03.to_csv('../data/nsfw_experiment3.csv',index=False)
+print('Saved ../data/nsfw_experiment3.csv')
