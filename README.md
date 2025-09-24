337fe175a69277c5c5d4ef79cac102345459e968
Huseyin Kaya <hkayabilisim@gmail.com>
2025-09-25 01:56:52 +0300

    feature: added Experiment 05

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 # Preprocessing
-The list of variables:
+Overview of some variables:
 
 |NSFW Data                            |AWARE (Article)     |AWARE (Code)             |MASCOTS (Paper)|This Repo (ELEKO 2025)  |
 |-------------------------------------|--------------------|-------------------------|---------------|------------------------|
 |replica                              |# of replicas       |num_containers           |$n_t$          |num_containers          |
-|expected_tps                         |arrival rate        |arrival_rate             |$l_t$          |arrival_rate            |        
-|cpu_usage ($\times$ 1000 milicpu)    |n/a                 |avg_cpu_usage            |$\hat{c}_t$    |avg_cpu_usage           |
 |cpu ($\times$ 100 milicpu)           |n/a                 |cpu_shares_per_container |$c_t$          |cpu_shares_per_container|
+|cpu_usage ($\times$ 1000 milicpu)    |n/a                 |avg_cpu_usage            |$\hat{c}_t$    |avg_cpu_usage           |
 |n/a                                  |cpu utilization     |avg_cpu_util             |$u_t$          |avg_cpu_util            |
+|expected_tps                         |arrival rate        |arrival_rate             |$l_t$          |arrival_rate            |
 |n/a                                  |n/a                 |n/a                      |$\lambda_t$    |arrival_change_rate     |
 |num_request                          |throughput          |processing_rate          |$\hat{l}_t$    |processing_rate         |
-|n/a                                  |throughtput preservation ratio|data_processing_rate|$\mu_t$   |data_processing_rate    |
-|...                                  |...                 |...                      |...            |...                     |
+|n/a                                  |throughtput preservation ratio|data_processing_rate|$\mu_t=\hat{l}_t/l_t$   |data_processing_rate    |
+|response_time                        |request-serving latency       |latency             |$\rho_t$  |latency                 |
+|n/a                                  |latency SLO         |slo_latency              |$\bar{\rho}$   |slo_latency             |
 
 
 # Experiments
@@ -56,6 +57,26 @@ Based on real-world NSFW data. We first filter [nsfw_raw.csv](data/nsfw_raw.csv)
 |Code| [experiment02.py](code/experiment02.py)|
 |What is expected?|Unlike the first experiment, the agent should converge to action=1 since there is a reverse correlation between *num_containers* and *cpu_avg_usage* and our reward is to maximize *cpu_avg_usage*.|
 
+
+## Experiment05
+Features:
+* workload trace file is scanned in a periodic manner meaning that if the agent somes to the end of the workload trace, it goes back to the start of the trace. In this way, step size can be much longer than trace length.
+* Award/Lost plot is saved to results folder. It creates the folder if not exists.
+* preprocessing.py saves experiment5.csv to data folder.
+
+|Key|Value|
+|---|-----|
+|All variables| *num_containers*, *cpu_shares_per_container*, *avg_cpu_usage*, *cpu_utilization*, *data_processing_rate*, *arrival_rate*, *previous_arrival_rate*, *processing_rate*, *arrival_change_rate*, *latency*|
+|Action Space (Independent Variables)| *num_containers*: {1,2,3}, *cpu_shares_per_container*: {4000,4100,...,4900} (milicpu)|
+|State| all variables|
+|Control Variables| *arrival_rate* from the periodic workload trace  [14, 7, 5, 4, 3, 4, 3, 3, 2, 2, 3, 4, 6, 10, 14, 18, 21, 22, 24, 25, 27, 27, 27, 26, 27, 27, 27, 26, 25, 24, 25, 24, 23, 22, 22, 22, 23, 23, 24, 24, 26, 27, 30, 29, 26, 23, 19, 17] |
+|Dependent/Measured Variables|all variables except action and control variables|
+|Reward| Same with MASCOTS 2025|
+|Data| [nsfw_experiment5.csv](data/nsfw_experiment5.csv)|
+|Preprocessing| Check [preprocessing.py](code/preprocessing.py)
+|Code| [experiment05.py](code/experiment05.py)|
+|Hyperparameters|See source code|
+|What is expected?|Increasing reward and decreasing loss|
 
 
 
---
+++ code/experiment05.py
@@ -0,0 +1,407 @@
+from collections import namedtuple, deque
+import random
+import math
+import numpy as np
+from typing import Dict, Tuple
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import pandas as pd
+import itertools
+from pathlib import Path
+
+# State vektöründe sıfırıncı index hangisiydi, besinci index neydi gibi seylerden kaçmak için
+# mumkun mertebe dict kullandım, en son torch'a girerken tensor'a çeviriyorum.
+STATE_KEYS = ['num_containers',
+              'cpu_shares_per_container',
+              'avg_cpu_usage',
+              'cpu_utilization',
+              'data_processing_rate',
+              'arrival_rate',
+              'previous_arrival_rate',
+              'processing_rate',
+              'arrival_change_rate',
+              'latency']
+# DQN'in replay memory'si için transition tanımı
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+# Episode sayisi
+NUM_EPISODES = 200
+# Step sayisi
+NUM_STEPS = 128
+# Tekrarlanabilirlik için random seed
+RANDOM_SEED = 42
+# Container sayisi, min, max, ve initial
+MIN_NUM_CONTAINERS = 1
+MAX_NUM_CONTAINERS = 3
+# CPU allocation in milicpus (verticala hazırlık)
+MIN_CPU_SHARES_PER_CONTAINER = 4000
+MAX_CPU_SHARES_PER_CONTAINER = 4900
+# 100 milicpuluk bir discretizasyon var
+INCR_CPU_SHARES_PER_CONTAINER = 100
+MIN_CPU_SCALING = 0
+MAX_CPU_SCALING = 9
+MAX_ARRIVAL_RATE = 30
+SLO_LATENCY = 1.5 # seconds
+MAX_LATENCY = 4.5 # seconds
+SLO_CPU_UTILIZATION = 0.8
+# Aksiyon: kaç konteyner varsa o kadar aksiyon var
+NUM_ACTIONS = (MAX_NUM_CONTAINERS - MIN_NUM_CONTAINERS + 1) * (MAX_CPU_SCALING - MIN_CPU_SCALING + 1)
+# State: STATE_KEYS'teki eleman sayısı kadar. Network'e key sırası ile giriyor.
+NUM_STATES = len(STATE_KEYS)
+# Replay memory'den batch_size kadar random örnek alıp train ediyoruz.
+BATCH_SIZE = 128
+# replay memory'nin max büyüklüğü. Yeni bir transition eklenirse en eski siliniyor. (deque ile yapmışlar)
+REPLAY_MEMORY_SIZE = 1024
+# Bunu cok bilmiyorum, DQN training sırasında kullanılıyor.
+GAMMA = 0.99
+# Epsilon-greedy için başlangıç, bitiş ve decay hızı
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 4000
+# Kaç episode'da bir target network'ü policy network ile aynı yapalım.
+TARGET_UPDATE = 200
+
+def action_tensor_to_dict(action: torch.Tensor) -> Dict:
+    '''Network'den tensor olarak çıkan sıfır indeksli aksiyonu dict'e çeviriyoruz.'''
+
+    action_index = action.item()
+
+    # action_index'ini (horizontal, vertical) tuple'ına çeviriyoruz
+    horizontal = tuple(range(MIN_NUM_CONTAINERS, MAX_NUM_CONTAINERS +1))
+    vertical = tuple(range(MIN_CPU_SHARES_PER_CONTAINER, MAX_CPU_SHARES_PER_CONTAINER + 1, INCR_CPU_SHARES_PER_CONTAINER))
+    action_tuples = list(itertools.product(horizontal, vertical))
+
+    action_dict = {'scale_to': action_tuples[action_index]}
+    return action_dict
+
+def state_to_tensor(state: Dict) -> torch.Tensor:
+    '''State dict'ini torch tensor'a çeviriyoruz. Burada listeyi ekstra bir [] içine koyuyoruz ki
+    birden fazla state'i birleştirince batch dimension'i oluşsun.'''
+
+    return torch.FloatTensor([list(state.values())])
+
+def normalize_state(state: Dict) -> Dict:
+    '''State normalizasyonunu özellikle dict üzerinde yapıyoruz ki sayısal indeksler neydi pesinde kosmayalim'''
+
+    normalized_state = state.copy()
+    normalized_state['num_containers'] /= MAX_NUM_CONTAINERS
+    normalized_state['cpu_shares_per_container'] /=  MAX_CPU_SHARES_PER_CONTAINER
+    normalized_state['avg_cpu_usage'] /= 1000
+    normalized_state['arrival_rate'] /= MAX_ARRIVAL_RATE
+    normalized_state['previous_arrival_rate'] /= MAX_ARRIVAL_RATE
+    normalized_state['processing_rate'] /= MAX_ARRIVAL_RATE
+    
+    return normalized_state
+
+
+def state_to_reward(state: Dict) -> float:
+    beta = 1.0 / MAX_LATENCY
+    w = 0.8
+    perf_reward = 0
+    if state['latency'] > SLO_LATENCY:
+        perf_reward = 1 - np.exp(beta * state['latency'])
+
+    cost_reward = 0
+    if state['cpu_utilization'] < SLO_CPU_UTILIZATION:
+        cost_reward = 1 - np.exp(1 - state['cpu_utilization'])
+
+    return w * perf_reward + (1 - w) * cost_reward
+
+def print_step_info(episode: int, step: int, state: Dict, action: Dict, next_state: Dict, reward: float, loss: float, exploration: bool):
+    '''Diagnostik amaçlı çeşitli bilgileri yazdırıyoruz.'''
+
+    if not hasattr(print_step_info, 'count'):
+        print_step_info.count = 0
+
+    def p_keys(d: Dict) -> str:
+        print(' '.join([f'{k:>{len(k)}}' for k, v in d.items()]))
+        print(' '.join(['-'*len(k) for k, v in d.items()]))
+
+
+    def p_values(d: Dict) -> str:
+        print(' '.join([f'{v:{len(k)}.3f}' if isinstance(v, float) else f'{v:{len(k)}d}' for k, v in d.items()]))
+
+
+    all_dicts = {'episode': episode, 'step': step}
+    for k, v in state.items():
+        all_dicts[k] = v
+    all_dicts['hor_action'] = action['scale_to'][0]
+    all_dicts['ver_action'] = action['scale_to'][1]
+    #for k, v in next_state.items():
+    #    all_dicts[f'next_{k}'] = v
+    all_dicts['reward'] = reward
+    all_dicts['policy_net_loss'] = loss
+    all_dicts['exploration'] = exploration
+
+    if print_step_info.count % NUM_STEPS == 0:
+        p_keys(all_dicts)
+    p_values(all_dicts)
+
+    print_step_info.count += 1
+
+
+class ReplayMemory(object):
+    '''deque ile süper basit bir replay memory implementasyonu
+    yapmışlar çok beğendim.'''
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+class DQNNetwork(nn.Module):
+    def __init__(self, num_states, num_actions):
+        '''Normalize edilmiş state batch vektörünü alıyor. Aksiyon skorlarını döndürüyor.
+        B x num_states -> B x num_actions şeklinde.'''
+        super(DQNNetwork, self).__init__()
+
+        self.fc1 = nn.Linear(num_states, 16)
+        self.fc2 = nn.Linear(16, 16)
+        self.fc3 = nn.Linear(16, num_actions)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        output = self.relu(self.fc1(x))
+        output = self.relu(self.fc2(output))
+        output = self.fc3(output)
+        return output
+
+
+class DQN:
+    def __init__(self, env, num_states, num_actions):
+        '''DQN implementasyonu, çok bilmiyorum ama birbirinin aynısı
+        iki network kullanması enteresan. Bir bakarsınız hocam.'''
+        self.env = env
+
+        self.num_states = num_states
+        self.num_actions = num_actions
+
+        self.policy_net = DQNNetwork(num_states, num_actions)
+        self.target_net = DQNNetwork(num_states, num_actions)
+
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+        # Target net'te hiçbir zaman train yapmyacağız.
+        self.target_net.eval()
+
+        self.optimizer = optim.RMSprop(self.policy_net.parameters())
+        self.memory = ReplayMemory(REPLAY_MEMORY_SIZE)
+
+        self.steps_completed = 0
+
+    def select_action(self, state: Dict) -> Tuple[torch.Tensor, bool]:
+        '''Explore-exploit için epsilon-greedy stratejisi kullanıyoruz.'''
+        sample = random.random()
+        eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * self.steps_completed / EPS_DECAY)
+        self.steps_completed += 1
+        if sample > eps_threshold:
+            with torch.no_grad():
+                # state dictionary'sini son anda normalize edip tensor'a çeviriyoruz.
+                state_tensor = state_to_tensor(normalize_state(state))
+                action = self.policy_net(state_tensor).argmax().view(1, 1)
+                exploration = False
+        else:
+            action = torch.tensor([[random.randrange(self.num_actions)]], dtype=torch.long)
+            exploration = True
+
+        # donen action sıfır indeksli.
+        return action, exploration
+
+    def optimize_model(self):
+        '''Bu kisma çok hakim değilim, siz bir goz atarsanız sevinirim.'''
+
+        transitions = self.memory.sample(BATCH_SIZE)
+        batch = Transition(*zip(*transitions))
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+        next_state_values = torch.zeros(BATCH_SIZE)
+        next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0].detach()
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * GAMMA) + reward_batch.squeeze()
+
+        # Compute Huber loss
+        criterion = nn.SmoothL1Loss()
+
+        loss = criterion(state_action_values.squeeze(), expected_state_action_values)
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        for param in self.policy_net.parameters():
+            param.grad.data.clamp_(-1, 1)
+        self.optimizer.step()
+
+        return loss.item()
+
+    def train(self, num_episodes, num_steps, verbose = False):
+        rewards = np.zeros((num_episodes, num_steps))
+        losses = np.zeros((num_episodes, num_steps))
+
+        for episode in range(num_episodes):
+            state = self.env.reset()
+
+            for step in range(num_steps):
+                action, exploration = self.select_action(state)
+                action_to_execute = action_tensor_to_dict(action)
+
+                # Episode'u yarıda bırakma durumu yok, o yuzden done burada bosta
+                next_state, reward, done = self.env.step(action_to_execute)
+
+                # Eğitimde kullanmak için verileri replay memory'e ekliyoruz.
+                self.memory.push(state_to_tensor(normalize_state(state)),
+                                 action,
+                                 state_to_tensor(normalize_state(next_state)),
+                                 torch.tensor([reward]))
+
+                # BATCH_SIZE kadar dolmayanan kadar eğitim yapamıyoruz.
+                # Sonrasında her step'de optimize çalışacak. Elde edilen loss'u
+                # kaydediyorum.
+                if len(self.memory) >= BATCH_SIZE:
+                    loss = self.optimize_model()
+                else:
+                    loss = math.nan
+
+                # Diagnostic amaçlı çeşitli bilgileri yazdırıyoruz.
+                if verbose:
+                    print_step_info(episode, step, state, action_to_execute, next_state, reward, loss, exploration)
+                rewards[episode, step] = reward
+                losses[episode, step] = loss
+
+                # next_state bir sonraki adımın state'i olacak. O anlamda state'e aslında action
+                # almadan önceki state diyebiliriz.
+                state = next_state
+
+            # Target network'ü policy network ile aynı yapıyoruz.
+            if episode % TARGET_UPDATE == 0:
+                self.target_net.load_state_dict(self.policy_net.state_dict())
+        return {'rewards': rewards, 'losses': losses}
+
+class RLEnvironment:
+
+    def __init__(self):
+        '''Bağımsız değişkenlerimiz bunlar.'''
+        self.num_containers = None
+        self.cpu_shares_per_container = None
+        self.workload_indx = None
+
+        self.nsfw_data = pd.read_csv('../data/nsfw_experiment5.csv')
+
+        self.workload = [
+            14, 7, 5, 4, 3, 4, 3, 3, 2, 2, 3, 4, 6, 10, 14, 18, 21, 22, 24, 25,
+            27, 27, 27, 26, 27, 27, 27, 26, 25, 24, 25, 24, 23, 22, 22, 22,
+            23, 23, 24, 24, 26, 27, 30, 29, 26, 23, 19, 17
+        ]
+
+
+    def get_rl_states(self) -> Dict:
+        filtered_data = self.nsfw_data.query(f'num_containers == {self.num_containers} and arrival_rate == {self.workload[self.workload_indx]} and cpu_shares_per_container == {self.cpu_shares_per_container}')
+
+        assert not filtered_data.empty, (
+            f"No data found for num_containers={self.num_containers}, "
+            f"arrival_rate={self.workload[self.workload_indx]}, "
+            f"cpu_shares_per_container={self.cpu_shares_per_container}"
+        )
+
+        sample = filtered_data.sample(1)
+        state = {k: sample[k].values[0] for k in STATE_KEYS}
+        
+        return state
+
+    def reset(self) -> Dict:
+        '''Her episode başında environment'ı resetliyoruz.'''
+        self.num_containers = random.choice(range(MIN_NUM_CONTAINERS, MAX_NUM_CONTAINERS+1))
+        self.cpu_shares_per_container = random.choice(range(MIN_CPU_SHARES_PER_CONTAINER, MAX_CPU_SHARES_PER_CONTAINER + 1, INCR_CPU_SHARES_PER_CONTAINER))
+        self.workload_indx = random.randint(0, len(self.workload) - 1)
+
+        state= self.get_rl_states()
+
+        return state
+
+    def step(self, action: Dict) -> Tuple[Dict, float, bool]:
+        # Execute the action
+        self.num_containers, self.cpu_shares_per_container = action['scale_to']
+
+
+        # Observe the next state
+        state = self.get_rl_states()
+
+        # calculate the reward
+        reward = state_to_reward(state)
+
+        # check if done
+        done = False
+
+        self.workload_indx += 1
+
+        # rewind to first workload
+        if self.workload_indx >= len(self.workload):
+            self.workload_indx = 0
+
+        return state, reward, done
+
+def plot_history(history, save_path = None):
+    import matplotlib.pyplot as plt
+    num_episodes = history['rewards'].shape[0]
+
+    episode_rewards =np.mean(history['rewards'], axis=1)
+    smoothed_episode_rewards = np.zeros_like(episode_rewards)
+    for i in range(num_episodes):
+        start_idx = max(0, i - 10)
+        smoothed_episode_rewards[i] = np.nanmean(episode_rewards[start_idx:i+1])
+
+    episode_losses = np.mean(history['losses'], axis=1)
+    smoothed_episode_losses = np.zeros_like(episode_losses)
+    for i in range(num_episodes):
+        start_idx = max(0, i - 10)
+        smoothed_episode_losses[i] = np.mean(episode_losses[start_idx:i+1])
+
+    fig, axes = plt.subplots(nrows=2, ncols=1, sharex=True)
+
+    ax1, ax2 = axes.flatten()
+
+    color = 'tab:red'
+    ax1.set_xlabel('episode')
+    ax1.set_ylabel('reward', color=color)
+    ax1.plot(episode_rewards, color=color)
+    ax1.plot(smoothed_episode_rewards, '--', color=color)
+    ax1.tick_params(axis='y', labelcolor=color)
+
+    color = 'tab:blue'
+    ax2.set_ylabel('loss', color=color)  # we already handled the x-label with ax1
+    ax2.plot(episode_losses, color=color)
+    ax2.plot(smoothed_episode_losses, '--', color=color)
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+        
+    if save_path:
+        file_path = Path(save_path)
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        print(f'Saved reward/loss plot {save_path}')
+        plt.savefig(save_path)
+
+if __name__ == "__main__":
+    '''Ana programımız budur.'''
+
+    random.seed(RANDOM_SEED)
+    np.random.seed(RANDOM_SEED)
+    torch.manual_seed(RANDOM_SEED)
+
+    env = RLEnvironment()
+
+    agent = DQN(env, NUM_STATES, NUM_ACTIONS)
+
+    history = agent.train(NUM_EPISODES, NUM_STEPS, verbose = True)
+
+    plot_history(history, save_path='../results/experiment05.pdf')
